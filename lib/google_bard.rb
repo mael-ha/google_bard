b3c423a30ca2c378fc0ef56a7362d352f319c20b
Mael Harnois <maelh@MBP-MAEL.local>
2023-05-22 14:50:14 +0800

    gem creation

---
+++ lib/google_bard.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "google_bard/version"
+
+module GoogleBard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/google_bard_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe GoogleBard do
+  it "has a version number" do
+    expect(GoogleBard::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
